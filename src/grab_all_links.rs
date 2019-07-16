120439062ccd3844bde691eb77085fd3c80f6ace
chck <shimekiri.today@gmail.com>
2019-07-17 02:16:49 +0900

    Add the code grabbing all links

---
+++ src/grab_all_links.rs
@@ -0,0 +1,20 @@
+extern crate reqwest;
+extern crate select;
+
+use select::document::Document;
+use select::predicate::Name;
+
+fn main() {
+    hacker_news("https://news.ycombinator.com");
+}
+
+fn hacker_news(url: &str) {
+    let resp = reqwest::get(url).unwrap();
+    assert!(resp.status().is_success());
+
+    Document::from_read(resp)
+        .unwrap()
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| println!("{}", x));
+}
